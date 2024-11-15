61dee5a1f3f7efd49af443a8ac627322709915e2
Zen Wang <zxc82990@gmail.com>
2024-11-15 23:19:07 +0700

    feat: add PRP token

---
+++ contracts/contracts/PermaReputationPoints.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PermaReputationPoints is ERC20("PermaReputationPoints", "PRP"), Ownable {
+    constructor(address owner) Ownable(owner) {
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}

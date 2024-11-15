41e867a27568423645e2040d3073f661dcdc9373
Irene Chang <yayinirene@gmail.com>
2024-11-16 03:41:55 +0700

    feat: Add Account and Actions components to Home

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from 'next/image';
+'use client';
+import Account from '@/components/Account';
+import Actions from '@/components/Actions';
 
 export default function Home() {
-	return <div className="page-home"></div>;
+	return (
+		<div className="page-home">
+			<Account />
+			<div className="py-6 pt-12 pb-20">
+				<Actions />
+			</div>
+		</div>
+	);
 }
